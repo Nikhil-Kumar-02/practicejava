a1e36ed70addf00f8431707c36f8605dc61f5c59
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-07-31 14:21:21 +0530

    first commit

---
+++ hellojava/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, java!");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt(); 
+        System.out.println("the entered input is " +  i);
+    }
+}
